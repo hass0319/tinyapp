0b776e1142fcac23d186b27ebaf5749e4101ca36
hass0319 <amro.abdrabou@hotmail.co.uk>
2022-02-22 06:04:54 +0000

    short urls

--- express_server.js
+++ express_server.js
@@ -9,11 +9,6 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
 app.listen(PORT, ()=> {
   console.log(`Example app listening on port ${PORT}!`);
 });
@@ -28,6 +23,11 @@ app.get("/urls.json", (req,res) => {// http://localhost:8080/urls.json
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
