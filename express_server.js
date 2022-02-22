8fc89b69d5a51f2359239a5d56f2fb2d1c70ecb3
hass0319 <amro.abdrabou@hotmail.co.uk>
2022-02-22 05:48:10 +0000

    urls index fix

--- express_server.js
+++ express_server.js
@@ -9,6 +9,11 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.listen(PORT, ()=> {
   console.log(`Example app listening on port ${PORT}!`);
 });
@@ -18,12 +23,16 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls.json", (req,res) => {// http://localhost:8080/urls.json
-
   res.json(urlDatabase);
 });
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.shortURL };
+  res.render("urls_show", templateVars);
 });
 
 // app.get("/set", (req, res) => {// http://localhost:8080/set
