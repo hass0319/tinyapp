e2fdcf2505add43dafa2f6df2535d2f2bcc5eb8b
hass0319 <amro.abdrabou@hotmail.co.uk>
2022-02-22 04:14:50 +0000

    commented example

--- express_server.js
+++ express_server.js
@@ -7,18 +7,27 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
 app.listen(PORT, ()=> {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/urls.json", (req,res) => {
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls.json", (req,res) => {// http://localhost:8080/urls.json
+
   res.json(urlDatabase);
 });
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+// app.get("/set", (req, res) => {// http://localhost:8080/set
+//   const a = 1;
+//   res.send(`a = ${a}`);
+// });
+// app.get("/fetch", (req, res) => {
+//   res.send(`a = ${a}`);
+// });
