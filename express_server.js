76f876544fd0c2f1ca034ff99b527f128e97fca2
hass0319 <amro.abdrabou@hotmail.co.uk>
2022-02-22 13:58:02 +0000

    initial-urls_new.ejs

--- express_server.js
+++ express_server.js
@@ -32,6 +32,6 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  console.log(`req.params.longURL=> `, urlDatabase[req.params.shortURL]);
+  // console.log(`req.params.longURL=> `, urlDatabase[req.params.shortURL]);
   res.render("urls_show", templateVars);
 });
