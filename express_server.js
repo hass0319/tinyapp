705496b0284226d773361258c980c248872a4f1d
hass0319 <amro.abdrabou@hotmail.co.uk>
2022-02-23 19:49:24 +0000

    cookieParser install

--- express_server.js
+++ express_server.js
@@ -2,9 +2,11 @@ const PORT = 8080;
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 app.set("view engine", "ejs");
 
 const urlDatabase = {
