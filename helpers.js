136397ad3784a693e9d2bbd0a2b2b994ed90a0fb
hass0319 <amro.abdrabou@hotmail.co.uk>
2022-03-03 17:15:41 +0000

    moved user and url databases to database file

--- helpers.js
+++ helpers.js
@@ -1,27 +1,5 @@
+const { users, urlDatabase } = require('./database');
 
-const urlDatabase = {
-  b6UTxQ: {
-    longURL: "https://www.tsn.ca",
-    userID: "aJ48lW"
-  },
-  i3BoGr: {
-    longURL: "https://www.google.ca",
-    userID: "aJ48lW"
-  }
-};
-
-const users = {
-  "userRandomID": {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "$2a$10$uBWN.OlMM/xrXF96N4BSzeENXrY7nYqxCW9fEeO8QdwJbudP4sMAm"
-  },
-  "user2RandomID": {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "$2a$10$XHBDkt1sGDey8MdxgsSEE.nsNMUBE/VnnqicRY3XeockrrKi6DJSO"
-  }
-};
 function generateRandomString() {
   const randomString = Math.random().toString(36).substring(2,9);
   return randomString;
@@ -53,4 +31,4 @@ function UrlsForUser(urlDatabase, userId) {
   return userUrls;
 }
 
-module.exports = {users, urlDatabase, getUserByEmail, insertUser, generateRandomString, UrlsForUser};
+module.exports = { getUserByEmail, insertUser, generateRandomString, UrlsForUser };
