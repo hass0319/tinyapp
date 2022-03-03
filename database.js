3f024e4c57eef93cab96e2b326a6cc9045e0c603
hass0319 <amro.abdrabou@hotmail.co.uk>
2022-03-03 17:14:14 +0000

    initial, added databases

---
+++ database.js
@@ -0,0 +1,25 @@
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW"
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW"
+  }
+};
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "$2a$10$uBWN.OlMM/xrXF96N4BSzeENXrY7nYqxCW9fEeO8QdwJbudP4sMAm"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "$2a$10$XHBDkt1sGDey8MdxgsSEE.nsNMUBE/VnnqicRY3XeockrrKi6DJSO"
+  }
+};
+
+module.exports = { urlDatabase, users };
